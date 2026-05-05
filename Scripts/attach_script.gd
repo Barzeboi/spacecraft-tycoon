6495fff2431fe4211455fe35baf7b47fa738443d
Barzeboi <berzenoki@gmail.com>
2026-05-05 03:44:04 -0500

    laid the foundation for something to work eventually
    Changed the area the mouse clicked to be found through looking at all the collisionsape's positions and comparing subtracting them by the mouse distance, still doesn't work right but im sure it will

--- Scripts/attach_script.gd
+++ Scripts/attach_script.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+@export var collision_distance: Vector2
+var collision_array: Array[Array]
+
 
 func _on_mouse_entered() -> void:
-	EventCall.emit_signal("placement", null, true, global_position)
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	for child in get_children():
+		collision_distance = child.position - mouse_pos
+		collision_array.append([child, collision_distance])
+	collision_array.sort_custom(func(a,b): return a[1] < b[1])
+	EventCall.emit_signal("placement", null, true, collision_array[0][0].global_position)
 	print(true)
-
 
 func _on_mouse_exited() -> void:
 	EventCall.emit_signal('placement', null, false, global_position)
+	collision_array.clear()
 	print(false)
