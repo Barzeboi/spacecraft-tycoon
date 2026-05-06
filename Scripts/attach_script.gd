4c764ca492b2506b4a93196e8f9e22bde517f51e
Barzeboi <berzenoki@gmail.com>
2026-05-05 20:22:52 -0500

    placing works now

--- Scripts/attach_script.gd
+++ Scripts/attach_script.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
-@export var collision_distance: Vector2
+@export var collision_distance: float
 var collision_array: Array[Array]
 
 
 func _on_mouse_entered() -> void:
 	var mouse_pos: Vector2 = get_global_mouse_position()
 	for child in get_children():
-		collision_distance = child.position - mouse_pos
+		collision_distance = child.global_position.distance_to(mouse_pos)
 		collision_array.append([child, collision_distance])
 	collision_array.sort_custom(func(a,b): return a[1] < b[1])
 	EventCall.emit_signal("placement", null, true, collision_array[0][0].global_position)
